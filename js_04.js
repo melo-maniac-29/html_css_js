aba52a7e20f61fcda852108a555c74e3aed5b31f
melo-maniac-29 <allenbobby2003@gmail.com>
2024-08-22 00:01:07 +0530

    done

---
+++ js_04.js
@@ -0,0 +1,15 @@
+    let arr=["allen","20",{
+        name:"allen",
+        age:20,
+        cities:["kerala","banglore",{
+            country:"malaysia",
+            city:"kuala lumpur"
+        }]
+    }
+    ];
+
+    console.log(arr[0]);
+    console.log(arr[1]);
+    console.log(arr[2]);
+
+    console.log(arr[2].cities[2].country);
