bfa699250743f08c00e4bcb29f5b30f627f813f4
melo-maniac-29 <allenbobby2003@gmail.com>
2024-08-25 00:19:23 +0530

    done

---
+++ js/js_12.js
@@ -0,0 +1,22 @@
+class rectangle{
+    constructor(width,height,color){
+        this.width=width;
+        this.height=height;
+        this.color=color;
+    }
+
+
+area() {
+    const area = this.width * this.height;
+      return area;
+}
+
+paint () {
+         console.log(`Painting with color ${this.color}`);
+    }
+}
+
+
+const rect = new rectangle(2, 4)
+const area = rect.area();
+console.log(area)
