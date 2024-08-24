5b2cd74d94bc42e69d207ae1ee5a4e62be0f9913
melo-maniac-29 <allenbobby2003@gmail.com>
2024-08-25 01:13:20 +0530

    done

--- js/js_12.js
+++ js/js_12.js
@@ -17,6 +17,41 @@ paint () {
 }
 
 
-const rect = new rectangle(2, 4)
+const rect = new rectangle(2, 4,"red");
 const area = rect.area();
+rect.paint();
 console.log(area)
+
+
+
+
+
+
+class calculator{
+    constructor(a,b){
+        this.a=a;
+        this.b=b;
+    }
+
+    add(){
+        return this.a+this.b;
+    }
+
+    sub(){
+        return this.a-this.b;
+    }
+
+    div(){
+        return this.a/this.b;
+    }
+
+    mul(){
+        return this.a*this.b;
+    }
+}
+
+const calc = new calculator(2,3);
+console.log(calc.add());
+console.log(calc.sub());
+console.log(calc.div());
+console.log(calc.mul());
